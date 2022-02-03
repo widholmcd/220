7232e498338343daebdc2311d3a6baaf9d521e33
widholmcd <widholmcd@g.cofc.edu>
2022-02-03 16:28:38 -0500

    HW3- Doing various arithmetic problems with loops and generating sequences with loops- CW

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -2,21 +2,19 @@
 Name: Calvin Widholm
 hw3.py
 
-Problem: Do various arithmetic- add later
+Problem: Do various arithmetic problems with loops and generating sequences with loops
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is entirely my own work.- Calvin Widholm (I certify)
 """
 
 
 def average():
     num_grades = eval(input("How many grades will you enter?"))
     initial_value = 0
-    for i in range(num_grades):
-        j = eval(input("Enter grade: "))
-        initial_value = initial_value + j
+    for _ in range(num_grades):
+        grade = eval(input("Enter grade: "))
+        initial_value = initial_value + grade
     avg_num = initial_value / num_grades
     print("average is", avg_num)
 
@@ -35,7 +33,7 @@ def newton():
     x_value = eval(input("What number do you want to square root?"))
     num_approx = eval(input("How many times should we improve the approximation?"))
     approx = x_value
-    for i in range(num_approx):
+    for _ in range(num_approx):
         approx = ((approx + (x_value / approx)) / 2)
     print("The square root is approximately ", approx)
 
@@ -47,18 +45,16 @@ def sequence():
         print(2 * round((j + .01) / 2) - 1)
 
 
-    #Come back to we need to resolve odd terms issue
-
-
-
 def pi():
-    pass
-   # terms = eval(input("How many terms in the series?"))
-   # for b in range(1, 1 + terms, 2):
-      #  for a in range(2, 1 + terms, 2):
-
-   # for i in range(terms):
-
+    terms = eval(input("How many terms in the series?"))
+    pi_over_2_approx = 1
+    for i in range(1, terms + 1, 1):
+        numerator = (2 * round((i + .01) / 2))
+        denominator = (2 * round((i + 1.01) / 2)) - 1
+        pi_solve = (numerator / denominator)
+        pi_over_2_approx = pi_over_2_approx * pi_solve
+    pi_approx = 2 * pi_over_2_approx
+    print(pi_approx)
 
 
 if __name__ == '__main__':
