0a14a9b018546c961c90c24228b122d5ccf498d2
widholmcd <widholmcd@g.cofc.edu>
2022-02-11 14:30:17 -0500

    HW 4- Using math and graphics library to create various objects and do approximations- CW

--- assignments/hw4/hw4.py
+++ assignments/hw4/hw4.py
@@ -1,16 +1,15 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Calvin Widholm
+hw4.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Using the math and graphics library to create various objects and do aprroximations
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is entirely my own work.- Calvin Widholm- I certify
 """
 
-from graphics import *
+import math
+from graphics import Rectangle, GraphWin, Point, Text, Circle
 
 
 def squares():
@@ -19,45 +18,121 @@ def squares():
     height = 400
     win = GraphWin("Clicks", width, height)
 
-    # number of times user can move circle
+    # number of times user can move square
     num_clicks = 5
 
     # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to add more squares")
     instructions.draw(win)
 
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    # builds a square
+    shape = Rectangle(Point(50, 50), Point(100, 100))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
-    # allows the user to click multiple times to move the circle
-    for i in range(num_clicks):
+    # allows the user to click multiple times to move the square
+    for _ in range(num_clicks):
         click = win.getMouse()
-        center = shape.getCenter()  # center of circle
+        center = shape.getCenter()  # center of square
 
-        # move amount is distance from center of circle to the
+        # move amount is distance from center of square to the
         # point where the user clicked
         change_x = click.getX() - center.getX()
         change_y = click.getY() - center.getY()
-        shape.move(change_x, change_y)
+        #shape.move(change_x, change_y)
+        shape1 = shape.clone()
+        shape1.move(change_x, change_y)
+        shape1.draw(win)
+
+    closing_remark = Text(Point(200,200), "Click again to close")
+    closing_remark.draw(win)
+
 
     win.getMouse()
     win.close()
 
 
 def rectangle():
-    pass
+    width = 400
+    height = 400
+    win = GraphWin("Rectangle", width, height)
+
+    point_a = win.getMouse()
+    point_b = win.getMouse()
+
+    shape = Rectangle(point_a, point_b)
+    shape.draw(win)
+    shape.setFill("green")
+
+    x_point_a = point_a.getX()
+    x_point_b = point_b.getX()
+    y_point_a = point_a.getY()
+    y_point_b = point_b.getY()
+
+    x_length = abs(x_point_b - x_point_a)
+    y_height = abs(y_point_b - y_point_a)
+    perimeter = (2 * x_length) + (2 * y_height)
+    area = x_length * y_height
+
+    perimeter_text = Text(Point(200, 320), "Perimeter:")
+    perimeter_text2 = Text(Point(200, 340), perimeter)
+    perimeter_text.draw(win)
+    perimeter_text2.draw(win)
+
+    area_text = Text(Point(200, 360), "Area:")
+    area_text2 = Text(Point(200, 380), area)
+    area_text.draw(win)
+    area_text2.draw(win)
+
+    closing_remark = Text(Point(200, 200), "Click again to close")
+    closing_remark.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
 def circle():
-    pass
+    width = 400
+    height = 400
+    win = GraphWin("Circle", width, height)
+
+    center = win.getMouse()
+    circumf_point = win.getMouse()
+
+    x_value_1 = center.getX()
+    x_value_2 = circumf_point.getX()
+    y_value_1 = center.getY()
+    y_value_2 = circumf_point.getY()
+
+    radius = math.sqrt(((x_value_2 - x_value_1) ** 2) + ((y_value_2 - y_value_1) ** 2))
+
+    shape = Circle(center, radius)
+    shape.draw(win)
+    shape.setFill("teal")
+
+    radius_text = Text(Point(200, 350), "Radius:")
+    radius_text2 = Text(Point(200, 370), radius)
+    radius_text.draw(win)
+    radius_text2.draw(win)
+
+    closing_remark = Text(Point(200,200), "Click again to close")
+    closing_remark.draw(win)
+
+    win.getMouse()
+    win.close()
+
 
 
 def pi2():
-    pass
+    terms = eval(input("Enter the number of terms to sum:"))
+    approx = 0
+    for i in range(0, 2 * terms, 2):
+        approx = (4 / (i + 1)) * ((-1) ** (i // 2)) + approx
+    print("pi approximation:", approx)
+    accuracy = abs(math.pi - approx)
+    print("accuracy:", accuracy)
 
 
 if __name__ == '__main__':
