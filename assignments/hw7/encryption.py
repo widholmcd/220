c88d4a0282d81d2c4e3762f718ce91becda5b18c
widholmcd <widholmcd@g.cofc.edu>
2022-03-04 16:19:27 -0500

    HW 7- Writing functions and opening and writing in files- CW

---
+++ assignments/hw7/encryption.py
@@ -0,0 +1,28 @@
+def encode(message: str, key: int):
+    length_message = len(message)
+    encrypted_message = ""
+    for i in range(length_message):
+        single_char = message[i]
+        encrypt = ord(single_char) + key
+        new_char = chr(encrypt)
+        encrypted_message = encrypted_message + new_char
+    return encrypted_message
+
+def encode_better(message: str, key: str):
+    length_message = len(message)
+    length_key = len(key)
+    elements = []
+    for i in range(length_message):
+        elements.append(message[i])
+    key_elements = []
+    for j in range(length_message):
+        key_elements.append(key[j % length_key])
+    encrypt_message = []
+    for k in range(length_message):
+        key_ordinal = ord(key_elements[k]) - 65
+        message_ordinal = ord(message[k]) - 65
+        encrypt_message_ordinal = (message_ordinal + key_ordinal) % 58
+        encrypt_message_char = chr(encrypt_message_ordinal + 65)
+        encrypt_message.append(encrypt_message_char)
+    final_message = "".join(encrypt_message)
+    return final_message
